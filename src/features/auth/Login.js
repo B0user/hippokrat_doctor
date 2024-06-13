b3f610910a2a7d81131b9226a45796b04d40845f
B0user <56859914+B0user@users.noreply.github.com>
2024-06-13 16:58:48 +0500

    login

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -3,9 +3,46 @@ import useAuth from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 import useInput from '../../hooks/useInput';
 import useToggle from '../../hooks/useToggle';
-
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import axios from '../../api/axios';
+
+
 const LOGIN_URL = '/auth';
+
+
+
+
+
+
+
+
+
+// function Copyright(props) {
+//     return (
+//       <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//         {'Copyright © '}
+//         <Link color="inherit" href="https://mui.com/">
+//           RN dev
+//         </Link>{' '}
+//         {new Date().getFullYear()}
+//         {'.'}
+//       </Typography>
+//     );
+//   }
+  
+
 
 const Login = () => {
     const { setAuth } = useAuth();
@@ -59,53 +96,78 @@ const Login = () => {
     }
 
     return (
-
-        <section style={{marginTop: "300px"}}>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>Войти</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username" className="form-label">Логин:</label>
-                <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComplete="off"
-                    {...userAttribs}
-                    className="form-control"
-                    required
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+            >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Вход в систему
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                {...userAttribs}
+                ref={userRef}
                 />
-
-                <label htmlFor="password" className="form-label">Пароль:</label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                    className="form-control"
-                    required
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Пароль"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
                 />
-                <br />
-                <button className="btn btn-primary">Войти</button>
-                <div className="align-items-center d-flex form-check mt-2">
-                    <input
-                    className="form-check-input"
-                        type="checkbox"
-                        id="persist"
-                        onChange={toggleCheck}
-                        checked={check}
-                    />
-                    <label htmlFor="persist" className="form-check-label">Доверять этому устройству?</label>
-                </div>
-            </form>
-            {/* <p>
-                Нужен аккаунт?<br />
-                <span className="line">
-                    <Link to="/register">Зарегистрироваться</Link>
-                </span>
-            </p> */}
-        </section>
+                <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Запомнить меня"
 
+                />
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                >
+                Войти
+                </Button>
+                {/* <Grid container>
+                    <Grid item xs>
+                        <Link href="#" variant="body2">
+                        Forgot password?
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link href="#" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Grid>
+                </Grid> */}
+            </Box>
+            </Box>
+            {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+        </Container>
     )
 }
 
-export default Login
+export default Login;
